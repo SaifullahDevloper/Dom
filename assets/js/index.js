57a64316179c047fab859ba4970efe59ae7c3e25
SaifullahDevloper <124183375+SaifullahDevloper@users.noreply.github.com>
2023-04-13 10:08:39 +0500

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+// prompt("Enter your Name");
+// alert("Enter your Father Name");
+// prompt("Enter your Email");
+// var b = prompt("Fayaz");
+// // document.write(b);
+// document.write(typeof b)
+
+
+// document.querySelector("h1").innerHTML = "Programmer";
+
+// document.querySelector("input").click();
+
+// document.getElementsByTagName();
+
+// document.getElementsByTagName("li")[2].style.color="red";
+// 'red'
+
+function changeColor(){
+    document.getElementById('title').style.color ='blue'
+}
+
+function login(){
+    var a = document.getElementById("email").value;
+    var b = document.getElementById("password").value;
+    alert("Your email " + a + " Your Password " + b);
+}
